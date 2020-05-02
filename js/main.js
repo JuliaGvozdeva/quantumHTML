60f88aac3fd59cad21ec11348f23266dfa8e293c
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2020-05-02 10:59:34 +0400

    master

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Нашли контейнер с select
+const selectElement = document.querySelectorAll("[data-select]");
+// Нашли реальный select
+const selectElementReal = document.querySelector("#selectReal");
+
+// Прослуиваем клик в контейнере с select
+[...selectElement].forEach(selectButtonDropdown =>
+    selectButtonDropdown.addEventListener("click", function (event) {
+        // Выбираем элемент из dropdown
+        console.log(event.target);
+        if (event.target.hasAttribute("data-select-item")) {
+            const itemTitle = event.target.getAttribute(["data-select-item"]);
+            event.target.closest("[data-select]").querySelector("[data-select-title]").textContent = itemTitle;
+            // Выбираем значение в реальном select
+            selectElementReal.value = itemTitle.toLowerCase();
+        } else {
+            // Открываем / скрываем dropdown
+            this.querySelector(".header-select__dropdown").classList.toggle("hidden");
+        }
+    })
+);
